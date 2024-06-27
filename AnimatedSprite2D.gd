d86bf16057d2eeaf26eafeca518bf7798828ed51
Strikerfury22 <71235406+Strikerfury22@users.noreply.github.com>
2024-06-27 16:03:12 +0200

    Added button to play animation

--- AnimatedSprite2D.gd
+++ AnimatedSprite2D.gd
@@ -1,10 +1,19 @@
 extends AnimatedSprite2D
 
+var activable = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play("Attack")
-
+	play("None")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#print(is_playing(),activable)
+	if activable:
+		activable = false
+		play("Attack")
+
+func _input(event):
+	# check is_action_just_pressed
+	if !activable and event.is_action_pressed("Reproducir"):
+		activable = true
